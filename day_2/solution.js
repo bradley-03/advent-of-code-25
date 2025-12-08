f0b60621c818147c3ace7b8b3cfb2f47f5eb7b1f
Bradley <104906904+bradley-03@users.noreply.github.com>
2025-12-08 14:11:56 +0000

    day 2 part 2

--- day_2/solution.js
+++ day_2/solution.js
@@ -14,6 +14,25 @@ function repeatingStrCheck(str) {
   return str.slice(0, halfLength) === str.slice(halfLength, str.length)
 }
 
+function repeatingPatternCheck(str) {
+  let valid = false
+
+  for (let i = 1; i <= str.length / 2; i++) {
+    let pattern = str.substring(0, i)
+    for (let j = 0; j < str.length; j += i) {
+      if (str.substring(j, j + i) !== pattern) {
+        valid = false
+        break
+      } else {
+        valid = true
+      }
+    }
+    if (valid) break
+  }
+
+  return valid
+}
+
 function solutionOne() {
   const ranges = input.split(",")
   let total = 0
@@ -29,4 +48,20 @@ function solutionOne() {
   return total
 }
 
+function solutionTwo() {
+  const ranges = input.split(",")
+  let total = 0
+
+  for (let range of ranges) {
+    const parsedRange = parseRange(range)
+    for (let i = parsedRange[0]; i <= parsedRange[1]; i++) {
+      if (repeatingPatternCheck("" + i)) {
+        total += i
+      }
+    }
+  }
+  return total
+}
+
 console.log(solutionOne())
+console.log(solutionTwo())
